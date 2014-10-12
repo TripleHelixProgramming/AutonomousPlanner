0e479a9500954c3534d48eadcddf6beb7ca0831e
Jared <dicarloj236@gmail.com>
2014-10-12 11:04:52 -0400

    better ui

--- src/autonomousplanner/Util.java
+++ src/autonomousplanner/Util.java
@@ -19,12 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  * A few useful methods.
@@ -224,23 +218,23 @@ public class Util {
      * @param yaxis
      */
     public static void makeGraph(SegmentGroup group, String name, String yaxis) {
-        XYSeriesCollection collection = new XYSeriesCollection();
-        XYSeries series = new XYSeries(name);
-
-        //make sure the lists are the same size/from same path
-        //add to series
-        for (int i = 0; i < group.s.size() - 1; i++) {
-            series.add(group.s.get(i).time, group.s.get(i).acc);
-        }
-        //add series to collection
-        collection.addSeries(series);
-
-        //graph and make window
-        JFreeChart chart = ChartFactory.createScatterPlot(name, "X", yaxis,
-                collection, PlotOrientation.VERTICAL, true, true, false);
-        ChartFrame frame = new ChartFrame(name, chart);
-        frame.pack();
-        frame.setVisible(true);
+//        XYSeriesCollection collection = new XYSeriesCollection();
+//        XYSeries series = new XYSeries(name);
+//
+//        //make sure the lists are the same size/from same path
+//        //add to series
+//        for (int i = 0; i < group.s.size() - 1; i++) {
+//            series.add(group.s.get(i).time, group.s.get(i).acc);
+//        }
+//        //add series to collection
+//        collection.addSeries(series);
+//
+//        //graph and make window
+//        JFreeChart chart = ChartFactory.createScatterPlot(name, "X", yaxis,
+//                collection, PlotOrientation.VERTICAL, true, true, false);
+//        ChartFrame frame = new ChartFrame(name, chart);
+//        frame.pack();
+//        frame.setVisible(true);
 
     }
 }
